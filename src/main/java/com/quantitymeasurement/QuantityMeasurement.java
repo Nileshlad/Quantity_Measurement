59ebd716408d80112a7982b4cf7677daec0b68c3
Nileshlad <ladnilesh1111@gmail.com>
2020-04-10 12:14:37 +0530

    Added: To welcome message

---
+++ src/main/java/com/quantitymeasurement/QuantityMeasurement.java
@@ -0,0 +1,7 @@
+package com.quantitymeasurement;
+
+public class QuantityMeasurement {
+    public static void main(String[] args) {
+        System.out.println("WELCOME QUALITY MEASUREMET PROBLEM");
+    }
+}
