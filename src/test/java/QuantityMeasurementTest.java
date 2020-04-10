1b05b9aeafba32e93375e6a1f82b20cd32bd3e54
Nileshlad <ladnilesh1111@gmail.com>
2020-04-10 12:37:28 +0530

    Added: To test case 1.2 equal operator use

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -3,7 +3,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class QuantityMeasurementTest {
-    //TEST CASE 1.1
+    //TEST CASE 1.1 EQUAL METHOD
     @Test
     public void toChecktheEqualValueTrue() {
         QuantityMeasurement qualityMeasurement = new QuantityMeasurement(0);
@@ -11,5 +11,15 @@ public class QuantityMeasurementTest {
         boolean areEqual = qualityMeasurement.equals(qualityMeasurement1);
         Assert.assertEquals(true, areEqual);
     }
+
+    //TEST CASE 1.2 EQUAL OPERATOR
+    @Test
+    public void toChecktheNotEqualValueFalse() {
+        QuantityMeasurement qualityMeasurement = new QuantityMeasurement(0);
+        QuantityMeasurement qualityMeasurement1 = new QuantityMeasurement(0);
+        boolean areEqual = (qualityMeasurement==qualityMeasurement1);
+        Assert.assertEquals(false, areEqual);
+    }
+
 
 }
